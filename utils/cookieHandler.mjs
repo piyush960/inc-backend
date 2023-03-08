6c3fd0aa4ea44baceaa405d4ffe66824749f7972
Abhishek Jadhav <79781388+AbhishekJadhav2002@users.noreply.github.com>
2023-03-08 15:34:44 +0530

    Remove SameSite option from cookie

--- utils/cookieHandler.mjs
+++ utils/cookieHandler.mjs
@@ -4,7 +4,7 @@ const options = {
     path: '/',
     secure: process.env.NODE_ENV === 'production',
     signed: true,
-    sameSite: 'Strict'
+    sameSite: 'Node'
 }
 
 function sendCookie(res, cookies, path) {
