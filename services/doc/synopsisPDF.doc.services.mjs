4871fa070ecdcadf2499407ce6ff7a8d4694aa18
Manas Kshatriya <manaskshatriya01@gmail.com>
2023-04-16 01:00:46 +0530

    header and footer

--- services/doc/synopsisPDF.doc.services.mjs
+++ services/doc/synopsisPDF.doc.services.mjs
@@ -1,22 +1,25 @@
-import path from 'path';
-import { readFileSync } from 'fs';
+import path from "path";
+import { readFileSync } from "fs";
 import PdfPrinter from "pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts.js";
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 var fonts = {
   Roboto: {
-    italics: Buffer.from(readFileSync(__dirname + '/static/ArialItalic.ttf'), 'base64'),
+    italics: Buffer.from(
+      readFileSync(__dirname + "/static/ArialItalic.ttf"),
+      "base64"
+    ),
   },
   Times: {
-    normal: 'Times-Roman',
-    bold: 'Times-Bold',
-    italics: 'Times-Italic',
-    bolditalics: 'Times-BoldItalic'
+    normal: "Times-Roman",
+    bold: "Times-Bold",
+    italics: "Times-Italic",
+    bolditalics: "Times-BoldItalic",
   },
-}
+};
 
-const printer = new PdfPrinter({ vfs: pdfFonts.pdfMake.vfs, ...fonts })
+const printer = new PdfPrinter({ vfs: pdfFonts.pdfMake.vfs, ...fonts });
 
 function createSynopsis(projects) {
   const docDefinition = {
@@ -29,55 +32,100 @@ function createSynopsis(projects) {
       contentAccessibility: true,
       documentAssembly: true,
     },
-    header: {
-      italics: true,
-      margin: [60, 20],
-      columns: [
+
+    header: function () {
+      return [
         {
-          text: "Pune Institute of Computer Technology, Pune-43",
-          alignment: "left",
-          width: "50%",
+          columns: [
+            {
+              text: "InC Synopsis",
+              fontSize: 10,
+              italics: "true",
+              alignment: "right",
+            },
+          ],
+          margin: [30, 20, 30, 0],
         },
+        ,
         {
-          text: "Impetus Synopsis: InC 2023",
-          alignment: "right",
-          width: "50%",
+          canvas: [
+            {
+              type: "line",
+              x1: 50,
+              y1: 10,
+              x2: 595 - 30,
+              y2: 10,
+              lineWidth: 1,
+            },
+          ],
         },
-      ],
-      lineHeight: 1.5,
-      decoration: "underline",
-      fontSize: 10,
-      canvas: [
-        {
-          type: "line",
-          x1: 0,
-          y1: 1,
-          x2: 495.28,
-          y2: 2,
-          lineWidth: 10,
-          lineColor: "#aaa",
-        },
-      ],
+      ];
     },
     footer: function (currentPage, pageCount) {
-      return {
-        text: currentPage.toString() + " of " + pageCount,
-        margin: [20, 0, 0, 0], // top, right, bottom, left
-        alignment: "center",
-        italics: true,
-      }
+      return [
+        {
+          canvas: [
+            {
+              type: "line",
+              x1: 50,
+              y1: 10,
+              x2: 595 - 30,
+              y2: 10,
+              lineWidth: 1,
+            },
+          ],
+        },
+        {
+          columns: [
+            {
+              text: "InC Synopsis",
+              fontSize: 10,
+              italics: "true",
+              alignment: "right",
+              margin: [20, 0, 0, 0],
+              alignment: "left",
+              italics: true,
+            },
+            {
+              text: currentPage.toString(),
+              fontSize: 10,
+              italics: "true",
+              alignment: "right",
+              margin: [20, 0, 0, 0],
+              alignment: "center",
+              italics: true,
+            },
+            {
+              text: "PICT , Pune",
+              fontSize: 10,
+              italics: "true",
+              alignment: "right",
+              margin: [20, 0, 0, 0],
+              alignment: "right",
+              italics: true,
+            },
+          ],
+          margin: [30, 10, 30, 0],
+        },
+      ];
     },
     content: [
       {
         toc: {
-          title: { text: "Contents", style: "header", alignment: "left", fontSize: 24, fontWeight: "bold", margin: [10, 10], marginBottom: 40 },
+          title: {
+            text: "Contents",
+            style: "header",
+            alignment: "left",
+            fontSize: 24,
+            fontWeight: "bold",
+            margin: [10, 10],
+            marginBottom: 40,
+          },
           textMargin: [5, 5, 5, 5],
           //textStyle: {italics: true},
           numberStyle: { bold: true },
           numberMargin: [10, 10, 10, 10],
           pagebreak: "after",
-
-
         },
       },
 
@@ -87,19 +135,36 @@ function createSynopsis(projects) {
         bold: true,
         marginBottom: 10,
         pageBreak: "before",
-
       },
       ...projects.map((project) => [
         {
+          
           text: `${project.projectId} : ${project.projectTitle} `,
           fontSize: 14,
           bold: true,
           marginBottom: 8,
           tocItem: true,
-
         },
         //{ text: `Project ID:`, fontSize: 12 },
-        { text: `Project Abstract: ${project.projectAbstract}`, fontSize: 12 },
+        {
+          columns: [
+            {
+              text: "Abstract: ",
+              fontSize: 12,
+              bold: true,
+              width: "auto",
+              margin: [0, 0, 10, 0],
+            
+            },
+            {
+              text: `${project.projectAbstract}`,
+              fontSize: 12,
+              width: "auto",
+              
+            }
+          ],
+        },
+
         {
           canvas: [
             {
@@ -112,7 +177,7 @@ function createSynopsis(projects) {
               lineColor: "#aaa",
             },
           ],
-          margin: [0, 5, 0, 10],
+          margin: [0, 10, 0, 10],
         }, // horizontal line between projects
       ]),
     ],
