237de74cc02c488db0de4ee72923ab2eddd80dce
Manas Kshatriya <manaskshatriya01@gmail.com>
2023-04-17 02:21:19 +0530

    final synopsis draft

--- services/doc/synopsisPDF.doc.services.mjs
+++ services/doc/synopsisPDF.doc.services.mjs
@@ -1,9 +1,8 @@
 import path from "path";
-import {
-  readFileSync
-} from "fs";
+import { readFileSync } from "fs";
 import PdfPrinter from "pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts.js";
+import { changeCase } from "../../utils/index.js";
 
 const __dirname = path.resolve();
 var fonts = {
@@ -23,17 +22,16 @@ var fonts = {
 
 const printer = new PdfPrinter({
   vfs: pdfFonts.pdfMake.vfs,
-  ...fonts
+  ...fonts,
 });
 
 function createSynopsis(projects) {
-  const AD = projects['APPLICATION DEVELOPMENT' ]
-    const CN = projects ['COMMUNICATION NETWORKS AND SECURITY SYSTEMS'] 
-    const DS = projects ['DIGITAL / IMAGE/ SPEECH / VIDEO PROCESSING'] 
-    const ES = projects ['EMBEDDED/VLSI SYSTEMS'] 
-    const ML = projects ['MACHINE LEARNING AND PATTERN RECOGNITION'] 
-    const OT = projects ['OTHERS'] 
-
+  const AD = projects["APPLICATION DEVELOPMENT"];
+  const CN = projects["COMMUNICATION NETWORKS AND SECURITY SYSTEMS"];
+  const DS = projects["DIGITAL / IMAGE/ SPEECH / VIDEO PROCESSING"];
+  const ES = projects["EMBEDDED/VLSI SYSTEMS"];
+  const ML = projects["MACHINE LEARNING AND PATTERN RECOGNITION"];
+  const OT = projects["OTHERS"];
 
   const docDefinition = {
     permissions: {
@@ -47,44 +45,52 @@ function createSynopsis(projects) {
     },
 
     header: function () {
-      return [{
-          columns: [{
+      return [
+        {
+          columns: [
+            {
               text: "Pune Institute of Computer Technology, Pune-43",
               fontSize: 10,
               italics: "true",
               alignment: "left",
             },
             {
-              text: "Impetus Synopsis: InC 2023",
+              text: "Concepts Synopsis: InC 2023",
               fontSize: 10,
               italics: "true",
               alignment: "right",
             },
           ],
           margin: [50, 20, 50, 0],
-        }, ,
+        },
+        ,
         {
-          canvas: [{
-            type: "line",
-            x1: 50,
-            y1: 10,
-            x2: 595 - 50,
-            y2: 10,
-            lineWidth: 1,
-          }, ],
+          canvas: [
+            {
+              type: "line",
+              x1: 50,
+              y1: 10,
+              x2: 595 - 50,
+              y2: 10,
+              lineWidth: 1,
+            },
+          ],
         },
       ];
     },
     footer: function (currentPage, pageCount) {
-      return [{
-          canvas: [{
-            type: "line",
-            x1: 50,
-            y1: 10,
-            x2: 595 - 30,
-            y2: 10,
-            lineWidth: 1,
-          }, ],
+      return [
+        {
+          canvas: [
+            {
+              type: "line",
+              x1: 50,
+              y1: 10,
+              x2: 595 - 30,
+              y2: 10,
+              lineWidth: 1,
+            },
+          ],
         },
         {
           text: currentPage.toString(),
@@ -100,9 +106,9 @@ function createSynopsis(projects) {
       {
         toc: {
           title: {
-            text: "Contents",
+            text: "Contents".toUpperCase(),
             style: "header",
-            alignment: "left",
+            alignment: "center",
             fontSize: 24,
             fontWeight: "bold",
             margin: [10, 10],
@@ -117,8 +123,8 @@ function createSynopsis(projects) {
       },
       // toc section
       {
-        text: "APPLICATION DEVELOPMENT",
-        fontSize: 24,
+        text: "Application development, Database, System Applications, Big Data".toUpperCase(),
+        fontSize: 18,
         bold: true,
         margin: [10, 20],
         pageBreak: "before",
@@ -129,13 +135,13 @@ function createSynopsis(projects) {
       // toc sub-sections
       ...AD.map((project) => [
         {
-          text: `${project.pid} : ${project.title} `,
+          text: `${project.pid} : ${changeCase("sentence", project.title)}`,
           fontSize: 14,
           bold: true,
           tocItem: true,
           tocMargin: [10, 0, 0, 0],
           marginBottom: 8,
-          tocStyle: {fontSize: 12 , alignment : "justify" , marginBottom: 10},
+          tocStyle: { fontSize: 12, alignment: "justify", marginBottom: 10 },
         },
         //{ text: `Project ID:`, fontSize: 12 },
         {
@@ -148,10 +154,10 @@ function createSynopsis(projects) {
               margin: [0, 0, 10, 0],
             },
             {
-              text: `${project.abstract}`,
+              text: `${project.abstract.trim()}`,
               fontSize: 12,
               width: "auto",
-              alignment : "justify"
+              alignment: "justify",
             },
           ],
         },
@@ -172,8 +178,8 @@ function createSynopsis(projects) {
         }, // horizontal line between projects
       ]),
       {
-        text: "COMPUTER NETWORKS",
-        fontSize: 24,
+        text: "Communication, Networking, Security, Blockchain, Cloud Computing".toUpperCase(),
+        fontSize: 18,
         bold: true,
         margin: [10, 20],
         pageBreak: "before",
@@ -184,13 +190,13 @@ function createSynopsis(projects) {
       // toc sub-sections
       ...CN.map((project) => [
         {
-          text: `${project.pid} : ${project.title} `,
+          text: `${project.pid} : ${changeCase("sentence", project.title)} `,
           fontSize: 14,
           bold: true,
           tocItem: true,
           tocMargin: [10, 0, 0, 0],
           marginBottom: 8,
-          tocStyle: {fontSize: 12 , alignment : "justify" , marginBottom: 10},
+          tocStyle: { fontSize: 12, alignment: "justify", marginBottom: 10 },
         },
         //{ text: `Project ID:`, fontSize: 12 },
         {
@@ -206,7 +212,7 @@ function createSynopsis(projects) {
               text: `${project.abstract}`,
               fontSize: 12,
               width: "auto",
-              alignment : "justify"
+              alignment: "justify",
             },
           ],
         },
@@ -227,117 +233,7 @@ function createSynopsis(projects) {
         }, // horizontal line between projects
       ]),
       {
-        text: "MACHINE LEARNING",
-        fontSize: 24,
-        bold: true,
-        margin: [10, 20],
-        pageBreak: "before",
-        tocItem: true,
-        alignment: "center",
-        tocStyle: { bold: true, fontSize: 16 },
-      },
-      // toc sub-sections
-      ...ML.map((project) => [
-        {
-          text: `${project.pid} : ${project.title} `,
-          fontSize: 14,
-          bold: true,
-          tocItem: true,
-          tocMargin: [10, 0, 0, 0],
-          marginBottom: 8,
-          tocStyle: {fontSize: 12 , alignment : "justify" , marginBottom: 10},
-        },
-        //{ text: `Project ID:`, fontSize: 12 },
-        {
-          columns: [
-            {
-              text: "Abstract: ",
-              fontSize: 12,
-              bold: true,
-              width: "auto",
-              margin: [0, 0, 10, 0],
-            },
-            {
-              text: `${project.abstract}`,
-              fontSize: 12,
-              width: "auto",
-              alignment : "justify"
-            },
-          ],
-        },
-
-        {
-          canvas: [
-            {
-              type: "line",
-              x1: 0,
-              y1: 5,
-              x2: 595 - 120,
-              y2: 5,
-              lineWidth: 0.5,
-              lineColor: "#aaa",
-            },
-          ],
-          margin: [0, 10, 0, 10],
-        }, // horizontal line between projects
-      ]),
-      {
-        text: "EMBEDDED SYSTEMS",
-        fontSize: 24,
-        bold: true,
-        margin: [10, 20],
-        pageBreak: "before",
-        tocItem: true,
-        alignment: "center",
-        tocStyle: { bold: true, fontSize: 16 },
-      },
-      // toc sub-sections
-      ...ES.map((project) => [
-        {
-          text: `${project.pid} : ${project.title} `,
-          fontSize: 14,
-          bold: true,
-          tocItem: true,
-          tocMargin: [10, 0, 0, 0],
-          marginBottom: 8,
-          tocStyle: {fontSize: 12 , alignment : "justify" , marginBottom: 10},
-        },
-        //{ text: `Project ID:`, fontSize: 12 },
-        {
-          columns: [
-            {
-              text: "Abstract: ",
-              fontSize: 12,
-              bold: true,
-              width: "auto",
-              margin: [0, 0, 10, 0],
-            },
-            {
-              text: `${project.abstract}`,
-              fontSize: 12,
-              width: "auto",
-              alignment : "justify"
-            },
-          ],
-        },
-
-        {
-          canvas: [
-            {
-              type: "line",
-              x1: 0,
-              y1: 5,
-              x2: 595 - 120,
-              y2: 5,
-              lineWidth: 0.5,
-              lineColor: "#aaa",
-            },
-          ],
-          margin: [0, 10, 0, 10],
-        }, // horizontal line between projects
-      ]),
-      {
-        text: "DIGITAL / IMAGE/ SPEECH / VIDEO PROCESSING",
+        text: "Image Processing, DSP, Multimedia".toUpperCase(),
         fontSize: 24,
         bold: true,
         margin: [10, 20],
@@ -349,13 +245,13 @@ function createSynopsis(projects) {
       // toc sub-sections
       ...DS.map((project) => [
         {
-          text: `${project.pid} : ${project.title} `,
+          text: `${project.pid} : ${changeCase("sentence", project.title)} `,
           fontSize: 14,
           bold: true,
           tocItem: true,
           tocMargin: [10, 0, 0, 0],
           marginBottom: 8,
-          tocStyle: {fontSize: 12 , alignment : "justify" , marginBottom: 10},
+          tocStyle: { fontSize: 12, alignment: "justify", marginBottom: 10 },
         },
         //{ text: `Project ID:`, fontSize: 12 },
         {
@@ -371,7 +267,7 @@ function createSynopsis(projects) {
               text: `${project.abstract}`,
               fontSize: 12,
               width: "auto",
-              alignment : "justify"
+              alignment: "justify",
             },
           ],
         },
@@ -391,8 +287,9 @@ function createSynopsis(projects) {
           margin: [0, 10, 0, 10],
         }, // horizontal line between projects
       ]),
+
       {
-        text: "OTHERS",
+        text: "Embedded systems, VLSI, IoT, Remote sensing".toUpperCase(),
         fontSize: 24,
         bold: true,
         margin: [10, 20],
@@ -402,15 +299,15 @@ function createSynopsis(projects) {
         tocStyle: { bold: true, fontSize: 16 },
       },
       // toc sub-sections
-      ...OT.map((project) => [
+      ...ES.map((project) => [
         {
-          text: `${project.pid} : ${project.title} `,
+          text: `${project.pid} : ${changeCase("sentence", project.title)} `,
           fontSize: 14,
           bold: true,
           tocItem: true,
           tocMargin: [10, 0, 0, 0],
           marginBottom: 8,
-          tocStyle: {fontSize: 12 , alignment : "justify" , marginBottom: 10},
+          tocStyle: { fontSize: 12, alignment: "justify", marginBottom: 10 },
         },
         //{ text: `Project ID:`, fontSize: 12 },
         {
@@ -426,7 +323,120 @@ function createSynopsis(projects) {
               text: `${project.abstract}`,
               fontSize: 12,
               width: "auto",
-              alignment : "justify"
+              alignment: "justify",
+            },
+          ],
+        },
+
+        {
+          canvas: [
+            {
+              type: "line",
+              x1: 0,
+              y1: 5,
+              x2: 595 - 120,
+              y2: 5,
+              lineWidth: 0.5,
+              lineColor: "#aaa",
+            },
+          ],
+          margin: [0, 10, 0, 10],
+        }, // horizontal line between projects
+      ]),
+
+      {
+        text: "Machine Learning, Pattern Recognition, Artificial Intelligence, Expert System".toUpperCase(),
+        fontSize: 18,
+        bold: true,
+        margin: [10, 20],
+        pageBreak: "before",
+        tocItem: true,
+        alignment: "center",
+        tocStyle: { bold: true, fontSize: 16 },
+      },
+
+      // toc sub-sections
+      ...ML.map((project) => [
+        {
+          text: `${project.pid} : ${changeCase("sentence", project.title)} `,
+          fontSize: 14,
+          bold: true,
+          tocItem: true,
+          tocMargin: [10, 0, 0, 0],
+          marginBottom: 8,
+          tocStyle: { fontSize: 12, alignment: "justify", marginBottom: 10 },
+        },
+        //{ text: `Project ID:`, fontSize: 12 },
+        {
+          columns: [
+            {
+              text: "Abstract: ",
+              fontSize: 12,
+              bold: true,
+              width: "auto",
+              margin: [0, 0, 10, 0],
+            },
+            {
+              text: `${project.abstract}`,
+              fontSize: 12,
+              width: "auto",
+              alignment: "justify",
+            },
+          ],
+        },
+
+        {
+          canvas: [
+            {
+              type: "line",
+              x1: 0,
+              y1: 5,
+              x2: 595 - 120,
+              y2: 5,
+              lineWidth: 0.5,
+              lineColor: "#aaa",
+            },
+          ],
+          margin: [0, 10, 0, 10],
+        }, // horizontal line between projects
+      ]),
+
+      {
+        text: "Others ( Bio-Signal Processing, Biomedical, Bioinformatics, etc.)".toUpperCase(),
+        fontSize: 24,
+        bold: true,
+        margin: [10, 20],
+        pageBreak: "before",
+        tocItem: true,
+        alignment: "center",
+        tocStyle: { bold: true, fontSize: 16 },
+      },
+      // toc sub-sections
+      ...OT.map((project) => [
+        {
+          text: `${project.pid} : ${changeCase("sentence", project.title)} `,
+          fontSize: 14,
+          bold: true,
+          tocItem: true,
+          tocMargin: [10, 0, 0, 0],
+          marginBottom: 8,
+          tocStyle: { fontSize: 12, alignment: "justify", marginBottom: 10 },
+        },
+        //{ text: `Project ID:`, fontSize: 12 },
+        {
+          columns: [
+            {
+              text: "Abstract: ",
+              fontSize: 12,
+              bold: true,
+              width: "auto",
+              margin: [0, 0, 10, 0],
+            },
+            {
+              text: `${project.abstract}`,
+              fontSize: 12,
+              width: "auto",
+              alignment: "justify",
             },
           ],
         },
