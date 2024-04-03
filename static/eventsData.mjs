5490ed112091af87695ee497fa49624445775b3c
Om Panchwate <86113092+ompanchwate@users.noreply.github.com>
2024-04-03 11:26:26 +0530

    slots updated (#29)
    * judge login resolved
    
    * changes
    
    * referal form api
    
    * judge_email updated
    
    * judge_form_fixed
    
    * referral added
    
    * judge-login
    
    * added members table
    
    * judge validation
    
    * verify-registration updated
    
    * slots updated
    
    * slots updated
    
    * synopsis
    
    * judges api updated
    
    * abstract update page
    
    * changes in email
    
    * concepts mail updated
    
    * concepts mail updated
    
    * slots updated

--- static/eventsData.mjs
+++ static/eventsData.mjs
@@ -106,12 +106,12 @@ const teamSize = new Map([
 
 const slotsData = {
     // Slots
-    '1': 'Friday 5th April (10:00 AM - 1:00 PM)' ,
-    '2': 'Friday 5th April (2:00 PM - 5:00 PM)' ,
-    '3': 'Friday 5th April (5:00 PM - 7:00 PM)' ,
-    '4': 'Saturday 6th April (9:00 AM - 12:00 PM)' ,
-    '5': 'Saturday 6th April (2:00 PM - 5:00 PM)' ,
-    '6': 'Saturday 6th April (5:00 PM - 7:00 PM)' 
+    '1': 'Friday 5th April (10:00 AM - 01:00 PM)' ,
+    '2': 'Friday 5th April (02:00 PM - 05:00 PM)' ,
+    '3': 'Friday 5th April (05:00 PM - 07:00 PM)' ,
+    '4': 'Saturday 6th April (10:00 AM - 01:00 PM)' ,
+    '5': 'Saturday 6th April (02:00 PM - 05:00 PM)' ,
+    '6': 'Saturday 6th April (05:00 PM - 07:00 PM)' 
 }
 
 const paymentLinks = new Map([
