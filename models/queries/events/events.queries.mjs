7217f3e9472121b775aee832e5499d08be5be339
Om Panchwate <86113092+ompanchwate@users.noreply.github.com>
2024-04-06 04:59:22 +0530

    updated synopsis (present one) (#33)
    * judge login resolved
    
    * changes
    
    * referal form api
    
    * judge_email updated
    
    * judge_form_fixed
    
    * referral added
    
    * judge-login
    
    * added members table
    
    * judge validation
    
    * verify-registration updated
    
    * slots updated
    
    * slots updated
    
    * synopsis
    
    * judges api updated
    
    * abstract update page
    
    * changes in email
    
    * concepts mail updated
    
    * concepts mail updated
    
    * slots updated
    
    * backe=up api
    
    * impetus mail updated
    
    * ontime changes
    
    * lab-allocations
    
    * updated synopsis

--- models/queries/events/events.queries.mjs
+++ models/queries/events/events.queries.mjs
@@ -39,7 +39,23 @@ function eventsQueries(tableName) {
 
     const getRegistrations = () => 'CALL getRegistrations(?);'
 
-    const getProjects = (data) => `SELECT title, ${data}_projects.pid ,  abstract , domain, mode FROM ${data}_projects INNER JOIN ${data}_group_info ON ${data}_projects.pid = ${data}_group_info.pid;`
+    // const getProjects = (data) => `SELECT title, ${data}_projects.pid ,  abstract , domain, mode FROM ${data}_projects INNER JOIN ${data}_group_info ON ${data}_projects.pid = ${data}_group_info.pid;`
+
+    const getProjects = (data) => `SELECT title, ${data}_projects.pid ,  abstract , domain, mode 
+    FROM ${data}_projects 
+    INNER JOIN ${data}_group_info ON ${data}_projects.pid = ${data}_group_info.pid 
+    WHERE ${data}_projects.pid NOT IN (
+        'CO-DS1011', 'CO-ML1100', 'CO-ML1115', 'CO-CN1017', 'CO-CN1014',
+        'CO-ML1015', 'CO-ML1066', 'CO-ML0001', 'CO-ML1088', 'CO-OT0020',
+        'CO-ML1048', 'CO-OT1053', 'CO-AD0012', 'CO-CN1012', 'CO-OT0027',
+        'CO-AD1022', 'CO-ML1058', 'CO-DS1003', 'CO-AD1032', 'CO-OT0025',
+        'CO-CN1008', 'CO-DS1024', 'CO-OT0017', 'CO-ML1073', 'CO-ML0002',
+        'CO-CN0003', 'CO-DS1026', 'CO-AD0007', 'CO-ML1033', 'CO-ML1116',
+        'CO-OT0040', 'CO-OT1056', 'CO-AD0010', 'CO-DS1014',
+        'CO-CN0004', 'CO-OT1043', 'CO-AD0028', 'CO-AD1011',
+        'CO-CN1009', 'CO-AD0004', 'CO-DS1016', 'CO-CN1013',
+        'CO-DS0033', 'CO-OT1039', 'CO-AD0020', 'CO-ML1118'
+    );`
 
     const getProject = ({ event_name, pid }) => `SELECT title, ${event_name}_projects.pid ,  abstract , domain, mode FROM ${event_name}_projects INNER JOIN ${event_name}_group_info ON ${event_name}_projects.pid = ${event_name}_group_info.pid WHERE ${event_name}_projects.pid IN (${pid});`
 
@@ -49,7 +65,7 @@ function eventsQueries(tableName) {
     const getProjectAbstractQ = (pid, event_name) => {
         return `SELECT title, abstract FROM ${event_name}_projects where pid = '${pid}'`;
     }
-    
+
     const updateProjectAbstractQ = (event_name, pid, abstract) => {
         const escapedAbstract = abstract.replace(/'/g, "\\'").replace(/"/g, '\\"');
         return `UPDATE \`${event_name}_projects\` SET abstract = '${escapedAbstract}' where pid = '${pid}' ;`
